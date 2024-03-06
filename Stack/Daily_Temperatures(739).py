f8ca781afde2184b1000d7b682bca30ef8dfc872
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2024-03-06 21:03:30 +0500

    Day 15

--- Stack/Daily_Temperatures(739).py
+++ Stack/Daily_Temperatures(739).py
@@ -1,12 +1,26 @@
 from collections import deque
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = deque()
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+       
+        # stack = deque()
+        # ans = [0]
+        # for i, temperature in enumerate(temperatures):
+        #     while stack and temperature > temperatures[stack[-1]]:
+        #         index = stack.pop()
+        #         ans[index] = i - index
+        #     stack.append(i)
+        # return ans
+        ans = [0] * len(temperatures)
+        stack = []  # a decreasing stack
+
+        for i, temperature in enumerate(temperatures):
+            while stack and temperature > temperatures[stack[-1]]:
+                index = stack.pop()
+                ans[index] = i - index
+            stack.append(i)
+
+        return ans
         
-        
-        
-        
-        
-        
-        
-        return []
+temperatures = [73,74,75,71,69,72,76,73]
+a =Solution()
+print(a.dailyTemperatures(temperatures))
